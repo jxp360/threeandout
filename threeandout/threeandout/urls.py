e60c712b6e6df754ac4766a3af3c4d5990e5090d
Grant Faiks <grant.faiks@axiosengineering.com>
2013-08-24 23:01:39 -0400

    Basic Picks Entry Form working

--- threeandout/threeandout/urls.py
+++ threeandout/threeandout/urls.py
@@ -16,6 +16,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     #url(r'^threeandout/', include('test_stats.urls', namespace="threeandout")),
-    url(r'^threeandout/', include('test_stats.urls')),
+    url(r'^threeandout/', include('test_stats.urls',namespace="threeandout")),
     
 )
