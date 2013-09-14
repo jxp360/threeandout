5b63592422f517e7d4d301d6c642dba12d123f0a
Jeff Pfeiffenberger <jxp360@gmail.com>
2013-09-14 10:27:36 -0400

    Added IP to allowed hosts

--- threeandout/threeandout/settings_ops.py
+++ threeandout/threeandout/settings_ops.py
@@ -32,7 +32,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['www.threeandoutfantasyfootball.com','threeandoutfantasyfootball.com']
+ALLOWED_HOSTS = ['www.threeandoutfantasyfootball.com','threeandoutfantasyfootball.com','107.22.174.163']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
